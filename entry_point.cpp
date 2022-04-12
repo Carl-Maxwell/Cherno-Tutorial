7e8d9a3d64200420d6aff64cb8d3b6794715aca9
= <carl.maxwell@gmail.com>
2022-04-12 20:01:01 +0100

    clean up entry_point.cpp

--- entry_point.cpp
+++ entry_point.cpp
@@ -75,24 +75,23 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  int WIDTH = 1280, HEIGHT = 720;
-  // int WIDTH = 1920, HEIGHT = 1080;
+  // window:
+  // int WIDTH = 1280, HEIGHT = 720;
+  // window = glfwCreateWindow(WIDTH, HEIGHT, "Urbarak 3d", NULL, NULL);
 
-  // glfwGetPrimaryMonitor() param before last
+  // borderless window:
+  int WIDTH = 1920, HEIGHT = 1080;
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Urbarak 3d", glfwGetPrimaryMonitor(), NULL);
 
-  window = glfwCreateWindow(WIDTH, HEIGHT, "Urbarak 3d", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
 
-  // Application::the().window = window;
-
   glfwMakeContextCurrent(window);
 
   // glfwSetKeyCallback(window, key_callback);
   // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
 
   //
   // Initialize glew
@@ -118,56 +117,7 @@ int main() {
   // Initialize graphics
   //
 
-  f32 positions[] = {
-   // Positions           // UV coords
-     -50.0f, -50.0f,          0.0f, 0.0f,
-      50.0f, -50.0f,          1.0f, 0.0f,
-      50.0f,  50.0f,          1.0f, 1.0f,
-     -50.0f,  50.0f,          0.0f, 1.0f
-  };
-
-  u32 indices[] = {
-    0, 1, 2,
-    2, 3, 0
-  };
-
-  const u32 number_of_vertices = 4;
-  const u32 vertice_components = 4;
-
-  VertexArray vertexArr;
-  VertexBuffer vertexBuff(positions, sizeof(f32) * number_of_vertices * vertice_components);
-
-  VertexBufferLayout layout;
-  layout.push<f32>(2);
-  layout.push<f32>(2);
-  vertexArr.AddBuffer(vertexBuff, layout);
-
-  IndexBuffer indexBuf(indices, 6);
-
-  glm::mat4 proj_matrix = glm::ortho(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 1.0f);
-  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-  glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-  glm::mat4 model_view_projection_matrix = proj_matrix * view * model;
-
-  // Load images
-
-  Texture texture("resources/images/badguy.png");
-  texture.bind();
-
-  // Shader stuff
-
   Shader shader("resources/shaders/vertex.glsl", "resources/shaders/fragment.glsl");
-
-  // unbind all that stuff
-
-  glBindVertexArray(0);
-  glUseProgram(0);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-  // 
-
   Renderer renderer;
 
   //
@@ -196,7 +146,7 @@ int main() {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    
+
 
     // render imgui
     ImGui::Render();
